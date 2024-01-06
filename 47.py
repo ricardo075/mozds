3577267bd8969a1735a02fedfb6d0166221563ba
ricardo075 <155269880+ricardo075@users.noreply.github.com>
2024-01-07 00:11:43 +0200

    Add files via upload

---
+++ 47.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.set_page_config(page_title='TEMAS DA MATERIA DE MATEMATICA')
+
+st.title('CALCULADORA PARA MATEMATICA')
+
+st.write('CALCULADORA DE EQUAÇAO QUADRATICA, DELTA X1 E X2[CLIQUE AQUI]'
+         '(https://542nbm7bpkiacruejlpnwt.streamlit.app/)')
+
+st.write('CALCULADORA DE CONJUNTOS')
+
+
+
+
+
